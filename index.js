fdfe1b4e5ecca1b8cd1f45243998d38c5acf4a5e
Invenst PhoneFive <invenstphonefive@dhcp020-200-058.wireless.buffalo.edu>
2021-11-13 20:18:02 -0500

    correcting typo

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var app = express();
 
+app.get("/" , (req,res)=>{
+    res.send("here")
+})
+
 app.get("/url", (req, res, next) => {
     res.json(["Hello World"]);
    });
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
  console.log("Server running on port " + PORT);
