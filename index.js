e2e1a0f3aa5779becbdef686354e139b7b7edfc6
Invenst PhoneFive <invenstphonefive@dhcp020-200-058.wireless.buffalo.edu>
2021-11-13 19:32:07 -0500

    dummy api

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello");
+var express = require("express");
+var app = express();
+
+app.get("/url", (req, res, next) => {
+    res.json(["Hello World"]);
+   });
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
